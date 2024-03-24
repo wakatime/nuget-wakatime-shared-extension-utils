8e93a1e6509a7f9c946aa928ced85718958ce9ae
Mateusz Chrupczalski <Mateusz.Chrupczalski@edwardsvacuum.com>
2024-03-24 18:03:06 +0000

    Enhance FlagHolder with flag bulk operations
    Added methods in FlagHolder class to add and remove list of flags at once. Moved the IsValidHeartbeat() method from CliHeartbeat to FlagHolder class to centralize the flag validation logic. This change improves flag manipulation and validation by making it more efficient and centralized.

--- src/WakaTime.Shared.ExtensionUtils/CliHeartbeat.cs
+++ src/WakaTime.Shared.ExtensionUtils/CliHeartbeat.cs
@@ -16,21 +16,6 @@ namespace WakaTime.Shared.ExtensionUtils
 
         #endregion
 
-        public bool IsValid()
-        {
-            bool hasEntity = HasFlag(FlagEntity.CliFlagName);
-            bool hasEntityType = HasFlag(FlagEntityType.CliFlagName);
-            bool hasTime = HasFlag(FlagTime.CliFlagName);
-            bool hasCategory = HasFlag(FlagCategory.CliFlagName);
 
-            if (!hasEntity) WakaTime.Logger.Error("Entity is required for sending heartbeat.");
-            if(!hasEntityType) WakaTime.Logger.Error("Entity type is required for sending heartbeat.");
-            if(!hasTime) WakaTime.Logger.Error("Time is required for sending heartbeat.");
-            if(!hasCategory) WakaTime.Logger.Error("Category is required for sending heartbeat.");
-
-            return hasEntity && hasEntityType && hasTime && hasCategory;
-        }
-
-        private bool HasFlag(string cliFlagName) => Flags.ContainsKey(cliFlagName);
     }
 }
--- src/WakaTime.Shared.ExtensionUtils/FlagHolder.cs
+++ src/WakaTime.Shared.ExtensionUtils/FlagHolder.cs
@@ -45,8 +45,18 @@ namespace WakaTime.Shared.ExtensionUtils
                 _flags.Add(flag.FlagUniqueName, flag);
             }
         }
+        
+        internal void AddFlags(IEnumerable<IFlag> flags, bool overwrite = true)
+        {
+            foreach (var flag in flags) AddFlag(flag, overwrite);
+        }
 
         public void RemoveFlag(IFlag flag) => RemoveFlag(flag.FlagUniqueName);
+        
+        internal void RemoveFlags(IEnumerable<IFlag> flags)
+        {
+            foreach (var flag in flags) RemoveFlag(flag);
+        }
 
         public void RemoveFlag(string flagUniqueName)
         {
@@ -60,8 +70,28 @@ namespace WakaTime.Shared.ExtensionUtils
             WakaTime.Logger.Debug($"Flag {flagUniqueName} exists. Removing.");
             _flags.Remove(flagUniqueName);
         }
+        
+        internal void RemoveFlags(IEnumerable<string> flagUniqueNames)
+        {
+            foreach (string flagUniqueName in flagUniqueNames) RemoveFlag(flagUniqueName);
+        }
 
+        public bool IsValidHeartbeat()
+        {
+            bool hasEntity = HasFlag(FlagEntity.CliFlagName);
+            bool hasEntityType = HasFlag(FlagEntityType.CliFlagName);
+            bool hasTime = HasFlag(FlagTime.CliFlagName);
+            bool hasCategory = HasFlag(FlagCategory.CliFlagName);
 
+            if (!hasEntity) WakaTime.Logger.Error("Entity is required for sending heartbeat.");
+            if(!hasEntityType) WakaTime.Logger.Error("Entity type is required for sending heartbeat.");
+            if(!hasTime) WakaTime.Logger.Error("Time is required for sending heartbeat.");
+            if(!hasCategory) WakaTime.Logger.Error("Category is required for sending heartbeat.");
+
+            return hasEntity && hasEntityType && hasTime && hasCategory;
+        }
+
+        private bool HasFlag(string cliFlagName) => Flags.ContainsKey(cliFlagName);
 
         
     }

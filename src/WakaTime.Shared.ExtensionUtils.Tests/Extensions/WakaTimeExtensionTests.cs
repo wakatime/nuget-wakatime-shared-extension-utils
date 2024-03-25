264207e27d12baf84999a34057ea63b6487fd7ab
Mateusz Chrupczalski <Mateusz.Chrupczalski@edwardsvacuum.com>
2024-03-25 18:36:34 +0000

    Add unit tests for WakaTime.Shared.ExtensionUtils
    Two new test classes for WakaTime.Shared.ExtensionUtils have been created: WakaTimeTests and WakaTimeExtensionTests. Additionally, a Globals static class is added for test data reuse. A logger stub is also included to help with testing. All of these additions aim to improve thorough and consistent testing of the WakaTime.Shared.ExtensionUtils project.

---
+++ src/WakaTime.Shared.ExtensionUtils.Tests/Extensions/WakaTimeExtensionTests.cs
@@ -0,0 +1,149 @@
+﻿using JetBrains.Annotations;
+using System;
+using Xunit;
+using WakaTime.Shared.ExtensionUtils;
+using WakaTime.Shared.ExtensionUtils.Extensions;
+using WakaTime.Shared.ExtensionUtils.Flags;
+
+namespace WakaTime.Shared.ExtensionUtils.Tests.Extensions
+{
+    [TestSubject(typeof(WakaTimeExtension))]
+    public class WakaTimeExtensionTests
+    {
+        private readonly WakaTime _wakaTimeInstance;
+        private const string TestFileName = "test.txt";
+        private const string AlternateProjectName = "AlternateProject";
+        private HeartbeatCategory _heartbeatCategory;
+        private readonly string _heartbeatCategoryText;
+        private EntityType _entityType;
+        private readonly string _entityTypeText;
+
+        public WakaTimeExtensionTests()
+        {
+            this._wakaTimeInstance = new WakaTime(Globals.Metadata, Globals.LoggerStub);
+            
+            this._heartbeatCategory = HeartbeatCategory.Debugging;
+            this._heartbeatCategoryText = Enum.GetName(typeof(HeartbeatCategory), _heartbeatCategory)?.ToLower();
+            
+            this._entityType = EntityType.App;
+            this._entityTypeText = Enum.GetName(typeof(EntityType), _entityType)?.ToLower();
+        }
+
+        [Fact]
+        public void CreateHeartbeat_ShouldSetBasicFlags()
+        {
+            var result = _wakaTimeInstance.CreateHeartbeat();
+            
+            Assert.NotNull(result);
+            Assert.NotNull(result.GetFlag(FlagKey.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagPlugin.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagCategory.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagEntityType.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagTime.CliFlagName));
+        }
+
+        [Fact]
+        public void CreateHeartbeat_ShouldSetFileName()
+        {
+            var result = _wakaTimeInstance.CreateHeartbeat(TestFileName);
+            
+            Assert.NotNull(result);
+            Assert.NotNull(result.GetFlag(FlagKey.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagPlugin.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagCategory.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagEntityType.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagTime.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagEntity.CliFlagName));
+            Assert.Equal(TestFileName, result.GetFlag(FlagEntity.CliFlagName)
+                                              .GetValue());
+        }
+
+        [Fact]
+        public void CreateHeartbeat_ShouldSetIsWrite()
+        {
+            var result = _wakaTimeInstance.CreateHeartbeat(TestFileName, true);
+            
+            Assert.NotNull(result);
+            Assert.NotNull(result.GetFlag(FlagKey.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagPlugin.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagCategory.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagEntityType.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagTime.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagEntity.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagWrite.CliFlagName));
+            
+            Assert.Equal(TestFileName, result.GetFlag(FlagEntity.CliFlagName)
+                                              .GetValue());
+            Assert.True(result.GetFlag(FlagWrite.CliFlagName).GetValue() == "true");
+        }
+
+        [Fact]
+        public void CreateHeartbeat_AlternateProjectSet_ShouldSetAlternateProject()
+        {
+            var result = _wakaTimeInstance.CreateHeartbeat(TestFileName, true, AlternateProjectName);
+            
+            Assert.NotNull(result);
+            Assert.NotNull(result.GetFlag(FlagKey.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagPlugin.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagCategory.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagEntityType.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagTime.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagEntity.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagWrite.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagProjectAlternate.CliFlagName));
+            Assert.Equal(TestFileName, result.GetFlag(FlagEntity.CliFlagName)
+                                              .GetValue());
+            Assert.True(result.GetFlag(FlagWrite.CliFlagName).GetValue() == "true");
+            Assert.Equal(AlternateProjectName, result.GetFlag(FlagProjectAlternate.CliFlagName)
+                                                     .GetValue());
+        }
+        
+        [Fact]
+        public void CreateHeartbeat_HeartbeatCategorySet_ShouldOverrideHeartbeatCategory()
+        {
+            var result = _wakaTimeInstance.CreateHeartbeat(TestFileName, true, AlternateProjectName, _heartbeatCategory, _entityType);
+
+            Assert.NotNull(result);
+            Assert.NotNull(result.GetFlag(FlagKey.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagPlugin.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagCategory.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagEntityType.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagTime.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagEntity.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagWrite.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagProjectAlternate.CliFlagName));
+            Assert.Equal(TestFileName, result.GetFlag(FlagEntity.CliFlagName)
+                                             .GetValue());
+            Assert.True(result.GetFlag(FlagWrite.CliFlagName).GetValue() == "true");
+            Assert.Equal(AlternateProjectName, result.GetFlag(FlagProjectAlternate.CliFlagName)
+                                                     .GetValue());
+            Assert.Equal(_heartbeatCategoryText, result.GetFlag(FlagCategory.CliFlagName)
+                                                        .GetValue());
+        }
+
+        [Fact]
+        public void CreateHeartbeat_EntityTypeSet_ShouldOverrideEntityType()
+        {
+            var result = _wakaTimeInstance.CreateHeartbeat(TestFileName, true, AlternateProjectName, _heartbeatCategory, _entityType);
+
+            Assert.NotNull(result);
+            Assert.NotNull(result.GetFlag(FlagKey.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagPlugin.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagCategory.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagEntityType.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagTime.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagEntity.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagWrite.CliFlagName));
+            Assert.NotNull(result.GetFlag(FlagProjectAlternate.CliFlagName));
+            Assert.Equal(TestFileName, result.GetFlag(FlagEntity.CliFlagName)
+                                             .GetValue());
+            Assert.True(result.GetFlag(FlagWrite.CliFlagName).GetValue() == "true");
+            Assert.Equal(AlternateProjectName, result.GetFlag(FlagProjectAlternate.CliFlagName)
+                                                     .GetValue());
+            Assert.Equal(_heartbeatCategoryText, result.GetFlag(FlagCategory.CliFlagName)
+                                                       .GetValue());
+            Assert.Equal(_entityTypeText, result.GetFlag(FlagEntityType.CliFlagName)
+                                                .GetValue());
+        }
+    }
+}
---
+++ src/WakaTime.Shared.ExtensionUtils.Tests/WakaTimeTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using JetBrains.Annotations;
+using WakaTime.Shared.ExtensionUtils.Exceptions;
+using WakaTime.Shared.ExtensionUtils.Extensions;
+using WakaTime.Shared.ExtensionUtils.Flags;
+using Xunit;
+
+namespace WakaTime.Shared.ExtensionUtils.Tests
+{
+    [TestSubject(typeof(WakaTime))]
+    public class WakaTimeTests
+    {
+        [Fact]
+        public void WhenNew_HasKeyFlag()
+        {
+            // Arrange
+            var wakaTime = new WakaTime(Globals.Metadata, Globals.LoggerStub);
+
+            // Act
+            var keyFlag = wakaTime.CommonFlags.GetFlag(FlagKey.CliFlagName);
+
+            // Assert
+            Assert.NotNull(keyFlag);
+        }
+        
+        [Fact]
+        public void WhenNew_HasPluginFlag()
+        {
+            // Arrange
+            var wakaTime = new WakaTime(Globals.Metadata, Globals.LoggerStub);
+
+            // Act
+            var projectFlag = wakaTime.CommonFlags.GetFlag(FlagPlugin.CliFlagName);
+
+            // Assert
+            Assert.NotNull(projectFlag);
+        }
+        
+        [Fact]
+        public void WhenNewAndFlagNotSet_HasNoProjectFlag()
+        {
+            // Arrange
+            var wakaTime = new WakaTime(Globals.Metadata, Globals.LoggerStub);
+
+            // Act
+            var projectFlag = wakaTime.CommonFlags.GetFlag(FlagProject.CliFlagName);
+
+            // Assert
+            Assert.Null(projectFlag);
+        }
+        
+        [Fact]
+        public void WhenHeartbeatsCreatedAndFlagMissing_ThrowsException()
+        {
+            // Arrange
+            var wakaTime = new WakaTime(Globals.Metadata, Globals.LoggerStub);
+
+            // Act
+            // Assert
+            Assert.Throws<AggregateException>(() => wakaTime.CreateHeartbeat().AddFlagLanguage("TestLanguage").Send(true));
+        }
+        
+        [Fact]
+        public void WhenHeartbeatsCreated_HasItemsInQueue()
+        {
+            // Arrange
+            var wakaTime = new WakaTime(Globals.Metadata, Globals.LoggerStub);
+
+            // Act
+            wakaTime.CreateHeartbeat().AddFlagEntity("TestEntity").AddFlagLanguage("TestLanguage").AddFlagProject("TestProject").Send(true);
+            wakaTime.CreateHeartbeat().AddFlagEntity("TestEntity").AddFlagLanguage("TestLanguage").AddFlagProject("TestProject").Send(true);
+            wakaTime.CreateHeartbeat().AddFlagEntity("TestEntity").AddFlagLanguage("TestLanguage").AddFlagProject("TestProject").Send(true);
+
+            // Assert
+            Assert.Equal(3, wakaTime.HeartbeatQueue.Count);
+        }
+    }
+}

e271a8ebed1dac900f37ffe196d0f8e0d43f2e44
Mateusz Chrupczalski <Mateusz.Chrupczalski@edwardsvacuum.com>
2024-03-26 22:51:36 +0000

    Add unit tests for BoolFlag and StringFlag classes
    This commit introduces unit tests for BoolFlag and StringFlag classes in WakaTime.Shared.ExtensionUtils. The tests cover various scenarios from setting and formatting values for CLI and JSON, to handling empty string values. These tests aim to ensure that the flag settings work as expected and maintain code quality.

---
+++ src/WakaTime.Shared.ExtensionUtils.Tests/Flags/BoolFlagTests.cs
@@ -0,0 +1,73 @@
+﻿using JetBrains.Annotations;
+using WakaTime.Shared.ExtensionUtils.Extensions;
+using WakaTime.Shared.ExtensionUtils.Flags;
+using Xunit;
+
+namespace WakaTime.Shared.ExtensionUtils.Tests.Flags
+{
+    [TestSubject(typeof(Flag<bool>))]
+    public class BoolFlagTests
+    {
+        private readonly WakaTime _wakaTime = new WakaTime(Globals.Metadata, Globals.LoggerStub);
+
+        [Fact]
+        public void AddBoolFlag_SetTrue_CorrectlyFormattedForCli()
+        {
+            var hb = _wakaTime.CreateHeartbeat()
+                              .AddFlagWrite()
+                              .AddFlagVerbose();
+
+            var write = hb.GetFlag(FlagWrite.Name.Cli);
+            var verbose = hb.GetFlag(FlagVerbose.Name.Cli);
+            
+            Assert.Equal("--write", write.GetFormattedForCli());
+            Assert.Equal("--verbose", verbose.GetFormattedForCli());
+        }
+        
+        [Fact]
+        public void AddBoolFlag_SetFalse_CorrectlyFormattedForCli()
+        {
+            var hb = _wakaTime.CreateHeartbeat()
+                              .AddFlagWrite(false)
+                              .AddFlagVerbose(false);
+
+            var write = hb.GetFlag(FlagWrite.Name.Cli);
+            var verbose = hb.GetFlag(FlagVerbose.Name.Cli);
+            
+            Assert.Equal(string.Empty, write.GetFormattedForCli());
+            Assert.Equal(string.Empty, verbose.GetFormattedForCli());
+        }
+
+        [Fact]
+        public void AddBoolFlag_SetTrue_CorrectlyFormattedStringValue()
+        {
+            var hb = _wakaTime.CreateHeartbeat()
+                              .AddFlagWrite()
+                              .AddFlagVerbose();
+            
+            var write = hb.GetFlag(FlagWrite.Name.Cli);
+            var verbose = hb.GetFlag(FlagVerbose.Name.Cli);
+            
+            Assert.Equal("true", write.GetValue());
+            Assert.Equal("true", verbose.GetValue());
+            Assert.Equal("true", write.GetValue(true));
+            Assert.Equal("true", verbose.GetValue(true));
+        }
+        
+        [Fact]
+        public void AddBoolFlag_SetFalse_CorrectlyFormattedStringValue()
+        {
+            var hb = _wakaTime.CreateHeartbeat()
+                              .AddFlagWrite(false)
+                              .AddFlagVerbose(false);
+            
+            var write = hb.GetFlag(FlagWrite.Name.Cli);
+            var verbose = hb.GetFlag(FlagVerbose.Name.Cli);
+            
+            Assert.Equal("false", write.GetValue());
+            Assert.Equal("false", verbose.GetValue());
+            Assert.Equal("false", write.GetValue(true));
+            Assert.Equal("false", verbose.GetValue(true));
+        }
+    }
+}
---
+++ src/WakaTime.Shared.ExtensionUtils.Tests/Flags/StringFlagTests.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using JetBrains.Annotations;
+using WakaTime.Shared.ExtensionUtils.Extensions;
+using WakaTime.Shared.ExtensionUtils.Flags;
+using Xunit;
+
+namespace WakaTime.Shared.ExtensionUtils.Tests.Flags
+{
+    [TestSubject(typeof(Flag<string>))]
+    public class StringFlagTests
+    {
+        private readonly WakaTime _wakaTime = new WakaTime(Globals.Metadata, Globals.LoggerStub);
+        private const string ProjectName = "MyTestProject";
+        private const string AlternateProjectName = "MyAlternateProject";
+        private const string ApiKey = "92DEF62E-54DC-4BFF-A67A-F2A2C1EA083D";
+        private const string ApiKeyObfuscated = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXX083D";
+
+        private readonly string _cliProjectSetExpected = $"{FlagProject.Name.Cli}\" \"{ProjectName}"; // --project" "MyTestProject
+        private readonly string _cliAlternateProjectSetExpected = $"{FlagProjectAlternate.Name.Cli}\" \"{AlternateProjectName}"; // --alternate-project" "MyAlternateProject
+        
+        private readonly string _jsonProjectSetExpected = $"\"{FlagProject.Name.Json}\": \"{ProjectName}\""; // "project": "MyTestProject"
+        private readonly string _jsonAlternateProjectSetExpected = $"\"{FlagProjectAlternate.Name.Json}\": \"{AlternateProjectName}\""; // "alternate_project": "MyAlternateProject"
+        
+        private readonly string _cliApiKeyObfuscatedExpected = $"{FlagKey.Name.Cli}\" \"{ApiKeyObfuscated}"; // --key" "XXXXXXXX-XXXX
+        
+        [Fact]
+        public void AddStringFlag_SetValue_CorrectlyFormattedForCli()
+        {
+            var hb = _wakaTime.CreateHeartbeat().AddFlagProject(ProjectName)
+                              .AddFlagProjectAlternate(AlternateProjectName);
+            
+            var projectFlag = hb.GetFlag(FlagProject.Name.Cli);
+            var alternateProjectFlag = hb.GetFlag(FlagProjectAlternate.Name.Cli);
+            
+            Assert.Equal(_cliProjectSetExpected, projectFlag.GetFormattedForCli());
+            Assert.Equal(_cliAlternateProjectSetExpected, alternateProjectFlag.GetFormattedForCli());
+        }
+        
+        [Fact]
+        public void AddStringFlag_SetValueObfuscate_CorrectlyFormattedForCli()
+        {
+            var hb = _wakaTime.CreateHeartbeat()
+                              .AddFlagKey(ApiKey);
+            
+            var keyFlag = hb.GetFlag(FlagKey.Name.Cli);
+            
+            Assert.Equal(_cliApiKeyObfuscatedExpected, keyFlag.GetFormattedForCli(true));
+        }
+        
+        [Fact]
+        public void AddStringFlag_NoValue_CorrectlyFormattedForCli()
+        {
+            var hb = _wakaTime.CreateHeartbeat().AddFlagProject(string.Empty)
+                              .AddFlagProjectAlternate(string.Empty);
+            
+            var projectFlag = hb.GetFlag(FlagProject.Name.Cli);
+            var alternateProjectFlag = hb.GetFlag(FlagProjectAlternate.Name.Cli);
+            
+            Assert.Equal(string.Empty, projectFlag.GetFormattedForCli());
+            Assert.Equal(string.Empty, alternateProjectFlag.GetFormattedForCli());
+        }
+        
+        [Fact]
+        public void AddStringFlag_SetValue_CorrectlyFormattedForJson()
+        {
+            var hb = _wakaTime.CreateHeartbeat().AddFlagProject(ProjectName)
+                              .AddFlagProjectAlternate(AlternateProjectName);
+            
+            var projectFlag = hb.GetFlag(FlagProject.Name.Cli);
+            var alternateProjectFlag = hb.GetFlag(FlagProjectAlternate.Name.Cli);
+            
+            Assert.Equal(_jsonProjectSetExpected,          projectFlag.GetFormattedForJson());
+            Assert.Equal(_jsonAlternateProjectSetExpected, alternateProjectFlag.GetFormattedForJson());
+        }
+        
+        [Fact]
+        public void AddStringFlag_NoValue_CorrectlyFormattedForJson()
+        {
+            var hb = _wakaTime.CreateHeartbeat().AddFlagProject(string.Empty)
+                              .AddFlagProjectAlternate(string.Empty);
+            
+            var projectFlag = hb.GetFlag(FlagProject.Name.Cli);
+            var alternateProjectFlag = hb.GetFlag(FlagProjectAlternate.Name.Cli);
+            
+            Assert.Equal(string.Empty,          projectFlag.GetFormattedForJson());
+            Assert.Equal(string.Empty, alternateProjectFlag.GetFormattedForJson());
+        }
+        
+        [Fact]
+        public void AddStringFlag_SetValue_CorrectlyFormattedStringValue()
+        {
+            var hb = _wakaTime.CreateHeartbeat().AddFlagProject(ProjectName)
+                              .AddFlagProjectAlternate(AlternateProjectName);
+            
+            var projectFlag = hb.GetFlag(FlagProject.Name.Cli);
+            var alternateProjectFlag = hb.GetFlag(FlagProjectAlternate.Name.Cli);
+            
+            Assert.Equal(ProjectName, projectFlag.GetValue());
+            Assert.Equal(AlternateProjectName, alternateProjectFlag.GetValue());
+        }
+        
+        [Fact]
+        public void AddStringFlag_NoValue_CorrectlyFormattedStringValue()
+        {
+            var hb = _wakaTime.CreateHeartbeat().AddFlagProject(string.Empty)
+                              .AddFlagProjectAlternate(string.Empty);
+            
+            var projectFlag = hb.GetFlag(FlagProject.Name.Cli);
+            var alternateProjectFlag = hb.GetFlag(FlagProjectAlternate.Name.Cli);
+            
+            Assert.Equal(string.Empty, projectFlag.GetFormattedForJson());
+            Assert.Equal(string.Empty, alternateProjectFlag.GetFormattedForJson());
+        }
+    }
+}
